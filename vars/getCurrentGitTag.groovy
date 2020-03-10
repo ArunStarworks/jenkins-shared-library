1f679e7486189335c9d6a28579f714cb4b7b26e9
ArunStarworks <arun.tbone@gmail.com>
2020-03-10 16:46:22 +0000

    semanticversioning updated

--- vars/getCurrentGitTag.groovy
+++ vars/getCurrentGitTag.groovy
@@ -6,7 +6,7 @@ def call (String TARGET_BRANCH)
 
 CURRENT_TAG = bat(
 
-script: "git tag --merged ${TARGET_BRANCH} | sort -r --version-sort | head -n1",
+script: "git tag --merged ${TARGET_BRANCH} | sort -r --version-sort",
 returnStdout: true
 ).trim()
 
