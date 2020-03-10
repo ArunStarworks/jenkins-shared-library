132ffd3100bb7503db894076bd2181623ac2719b
ArunStarworks <arun.tbone@gmail.com>
2020-03-10 12:51:01 +0000

    semanticversioning updated

--- vars/getCurrentBranchName.groovy
+++ vars/getCurrentBranchName.groovy
@@ -2,23 +2,12 @@
 
 def call (String gitUserName, String gitUserEmail, String sshAgentId)
 {
+
 sh "git config user.name ${gitUserName}"
 sh "git config user.email ${gitUserEmail}"
   
-if ("${env.GIT_BRANCH}" =~ "origin/.*")
-{
-   echo "Branch contains origin - New branch is ${TARGET_BRANCH}"
-}
-else if ("${env.GIT_BRANCH}" =~ "PR-*")
-{
-   echo "Origin does not exist in the branch name - Adding it in"
-   TARGET_BRANCH = "origin/" + TARGET_BRANCH
-   echo "New branch is ${TARGET_BRANCH}"
-}
 
-else
-{
 TARGET_BRANCH = "${env.GIT_BRANCH}"
-}
+
 return TARGET_BRANCH
 }
