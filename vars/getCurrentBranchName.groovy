09e808da98f99dd0dad1e9a8117f83ed737aa85d
ArunStarworks <arun.tbone@gmail.com>
2020-03-16 10:17:40 +0000

    test

--- vars/getCurrentBranchName.groovy
+++ vars/getCurrentBranchName.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/groovy
 
-def call (String gitUserName, String gitUserEmail, String sshAgentId)
+def shell(command) (String gitUserName, String gitUserEmail, String sshAgentId)
 {
 
-bat "git config user.name ${gitUserName}"
-bat "git config user.email ${gitUserEmail}"
+sh "git config user.name ${gitUserName}"
+sh "git config user.email ${gitUserEmail}"
   
 
 TARGET_BRANCH = "${env.GIT_BRANCH}"
